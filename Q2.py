1cad5d1bd1c4c7715ef222c1dda1df40d43618f7
bettytarkin13-dotcom <bettytarkin13@gmail.com>
2026-02-24 17:42:55 +0200

    #2

---
+++ Q2.py
@@ -0,0 +1,8 @@
+#q2
+
+
+def basic_math_list(a,b):
+    return [a+b,a-b,a/b,a*b]
+
+result=basic_math_list(10,2)
+print(result)
---
+++ main.py
@@ -0,0 +1,7 @@
+#q 1
+
+def print_number(start,stop):
+    for num in range(start,stop+1):
+      print(num)
+
+print_number(3,7)

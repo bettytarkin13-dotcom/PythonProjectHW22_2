62bd4bbc1c7420e2b4c4429856c09c71d6710134
bettytarkin13-dotcom <bettytarkin13@gmail.com>
2026-02-25 17:20:28 +0200

    #8

--- Bonus.py
+++ Bonus.py
@@ -3,21 +3,21 @@
 import random
 import time
 
-
 def get_random_between_1_10()->int:  # בוחרת מס אקראי בין 1-10
     return random.randint(1,10)
 def get_random_operation()->str:  #בוחרת פעולה אקראית מתוך הרשימה
     return random.choice(["+","-","*","%"])
 
-def calc_result(num1: int,operator: str,num2: int) ->int: #מחשבת את התוצאה לפי התוצאה שהתקבלה
-    if operator=="+":
-      return num1+num2
-    elif operator=="-":
-      return num1-num2
-    elif operator=="*":
-      return num1*num2
-    elif operator=="%":
-      return num1%num2
+def calc_result(num1: int, operator: str, num2: int) -> int:
+    match operator:
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "*":
+            return num1 * num2
+        case "%":
+            return num1 % num2
 
 num1=get_random_between_1_10()
 operator=get_random_operation()
